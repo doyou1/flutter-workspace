bd90805f3c85b964ea1296f997c9d07a20f5b0f1
doyou1 <56281493+doyou1@users.noreply.github.com>
2021-12-22 14:46:19 +0900

    sqflite & pageroute

---
+++ sqflite_example_2/README.md
@@ -0,0 +1,12 @@
+# sqflite_example_2
+
+<h2>reference</h2>
+<div><b>1. https://www.youtube.com/watch?v=UpKrhZ0Hppk&t=3s</b></div>
+<div><b>2. https://github.com/JohannesMilke/sqflite_database_example</b></div>
+
+
+# 결과물
+![result](./screenshots/sqflite_note_example.gif)
+
+<hr>
+- 소감 : 코드 너무 지저분하게 짠다
---
+++ sqflite_example_2/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sqflite_example_2/page/notes_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String title = 'Notes SQLite';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blueGrey.shade900,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+      ),
+      home: NotesPage(),
+    );
+  }
+}
