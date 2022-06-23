cece655c17c24f8fd04ae409edeaaf294e1c5139
doyou1 <tla149@daum.net>
2022-06-23 22:05:07 +0900

    add goal & wall

--- study/dots-game/dots_game_example/lib/util/game_painter.dart
+++ study/dots-game/dots_game_example/lib/util/game_painter.dart
@@ -6,9 +6,11 @@ class GamePainter extends CustomPainter {
   int columns;
   double cellSize;
 
-  GamePainter(this.point, this.rows, this.columns, this.cellSize);
+  GamePainter(this.point, this.goal, this.wall, this.rows, this.columns, this.cellSize);
 
   Point<int> point;
+  Point<int>? goal;
+  List<Point<int>>? wall;
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -17,9 +19,17 @@ class GamePainter extends CustomPainter {
       ..color = Colors.black
       ..strokeWidth = 2.0
       ..style = PaintingStyle.stroke;
-    // 채워짐(snake)
+
+    final blueFilled = Paint()
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill;
+
     final blackFilled = Paint()
       ..color = Colors.black
+      ..style = PaintingStyle.fill;
+
+    final redFilled = Paint()
+      ..color = Colors.red
       ..style = PaintingStyle.fill;
 
     final a = Offset(0.0, 0.0);
@@ -28,12 +38,30 @@ class GamePainter extends CustomPainter {
     // 전체화면 그림
     canvas.drawRect(rect, blackLine);
 
+    // 내 위치
     // 왼쪽위 (x, y)
     final pa = Offset(cellSize * point.x, cellSize * point.y);
     // 오른쪽아래 (x+1, y+1)
     final pb = Offset(cellSize * (point.x + 1), cellSize * (point.y + 1));
+    canvas.drawRect(Rect.fromPoints(pa, pb), blueFilled);
 
-    canvas.drawRect(Rect.fromPoints(pa, pb), blackFilled);
+    if(goal != null) {
+      // 골
+      // 왼쪽위 (x, y)
+      final ga = Offset(cellSize * goal!.x, cellSize * goal!.y);
+      // 오른쪽아래 (x+1, y+1)
+      final gb = Offset(cellSize * (goal!.x + 1), cellSize * (goal!.y + 1));
+      canvas.drawRect(Rect.fromPoints(ga, gb), redFilled);
+    }
+
+    if(wall != null) {
+      for(Point<int> w in wall!) {
+        final wa = Offset(cellSize * w.x, cellSize * w.y);
+        // 오른쪽아래 (x+1, y+1)
+        final wb = Offset(cellSize * (w.x + 1), cellSize * (w.y + 1));
+        canvas.drawRect(Rect.fromPoints(wa, wb), blackFilled);
+      }
+    }
   }
 
   @override
--- study/dots-game/dots_game_example/lib/view/dots_game_page.dart
+++ study/dots-game/dots_game_example/lib/view/dots_game_page.dart
@@ -1,3 +1,4 @@
+import 'package:dots_game_example/main.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -11,25 +12,50 @@ class DotsGamePage extends StatefulWidget {
 }
 
 class _DotsGamePageState extends State<DotsGamePage> {
+
   double widgetSize = 300.0;
+
   int rows = 10;
   int columns = 10;
+  late double cellSize = (widgetSize / rows).toDouble();
   late Point<int> point =
       Point<int>((rows / 2).toInt(), (columns / 2).toInt());
-  late double cellSize = (widgetSize / rows).toDouble();
+  late Point<int> goal = getGoal();
+  late List<Point<int>> wall = getWall();
+  int wallCount = 5;
 
-  // late Timer _timer;
-  // final int milliseconds = 200;
+  Point<int> getGoal() {
+    while(true) {
+      int randomX = Random().nextInt(rows-1);
+      int randomY = Random().nextInt(columns-1);
 
-  @override
-  void initState() {
-    super.initState();
-
-    // 0.2초마다 reprint
-    // _timer = Timer.periodic(Duration(milliseconds: milliseconds), (timer) {
-    //   _step();
-    // });
+      if(point.x == randomX && point.y == randomY) {
+        continue;
+      } else {
+        return Point<int>(randomX, randomY);
+      }
+    }
   }
+
+  List<Point<int>> getWall() {
+    List<Point<int>> wall = [];
+
+    while(true) {
+      if(wall.length == wallCount) return wall;
+      int randomX = Random().nextInt(rows-1);
+      int randomY = Random().nextInt(columns-1);
+
+      if(point.x == randomX && point.y == randomY) {
+        continue;
+      }
+      else if(goal.x == randomX && goal.y == randomY) {
+        continue;
+      } else {
+        wall.add(Point<int>(randomX, randomY));
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +67,7 @@ class _DotsGamePageState extends State<DotsGamePage> {
             width: widgetSize,
             height: widgetSize,
             child: CustomPaint(
-              foregroundPainter: GamePainter(point, rows, columns, cellSize),
+              foregroundPainter: GamePainter(point, goal, wall, rows, columns, cellSize),
             ),
           ),
           SizedBox(
@@ -58,6 +84,7 @@ class _DotsGamePageState extends State<DotsGamePage> {
                       onPressed: () {
                         setState(() {
                           moveToUp();
+                          checkGoal();
                         });
                       },
                       child: Icon(Icons.arrow_upward),
@@ -71,6 +98,7 @@ class _DotsGamePageState extends State<DotsGamePage> {
                       onPressed: () {
                         setState(() {
                           moveToLeft();
+                          checkGoal();
                         });
                       },
                       child: Icon(Icons.arrow_back),
@@ -82,6 +110,7 @@ class _DotsGamePageState extends State<DotsGamePage> {
                       onPressed: () {
                         setState(() {
                           moveToDown();
+                          checkGoal();
                         });
                       },
                       child: Icon(Icons.arrow_downward),
@@ -93,6 +122,7 @@ class _DotsGamePageState extends State<DotsGamePage> {
                       onPressed: () {
                         setState(() {
                           moveToRight();
+                          checkGoal();
                         });
                       },
                       child: Icon(Icons.arrow_forward),
@@ -110,24 +140,59 @@ class _DotsGamePageState extends State<DotsGamePage> {
   void moveToLeft() {
     int newX = point.x - 1;
     if (newX < 0) newX = 0;
-    point = Point<int>(newX, point.y);
+
+    if(isWall(Point<int>(newX, point.y))) {
+      return;
+    } else {
+      point = Point<int>(newX, point.y);
+    }
   }
 
   void moveToRight() {
     int newX = point.x + 1;
     if (newX > columns - 1) newX = columns - 1;
-    point = Point<int>(newX, point.y);
+
+    if(isWall(Point<int>(newX, point.y))) {
+      return;
+    } else {
+      point = Point<int>(newX, point.y);
+    }
   }
 
   void moveToUp() {
     int newY = point.y - 1;
     if (newY < 0) newY = 0;
-    point = Point<int>(point.x, newY);
+    if(isWall(Point<int>(point.x, newY))) {
+      return;
+    } else {
+      point = Point<int>(point.x, newY);
+    }
   }
 
   void moveToDown() {
     int newY = point.y + 1;
     if (newY > rows - 1) newY = rows - 1;
-    point = Point<int>(point.x, newY);
+    if(isWall(Point<int>(point.x, newY))) {
+      return;
+    } else {
+      point = Point<int>(point.x, newY);
+    }
+  }
+
+  void checkGoal() {
+    if(point.x == goal.x && point.y == goal.y) {
+      Navigator.of(context).pop();
+      Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+        return HomePage();
+      }));
+    }
+  }
+
+  bool isWall(Point<int> newPoint) {
+
+    for(Point<int> w in wall) {
+      if(newPoint.x == w.x && newPoint.y == w.y) return true;
+    }
+    return false;
   }
 }
