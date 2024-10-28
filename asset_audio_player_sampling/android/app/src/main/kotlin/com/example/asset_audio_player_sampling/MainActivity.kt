f99a7d374eb9449c9200fe578197148a9e16f688
doyou1 <tla149@daum.net>
2024-10-28 16:27:36 +0900

    asset audio file read & play - sampling complete

---
+++ asset_audio_player_sampling/android/app/src/main/kotlin/com/example/asset_audio_player_sampling/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.asset_audio_player_sampling
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ asset_audio_player_sampling/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    AudioPlayer _audioPlayer = AudioPlayer();
+
+    Future<void> _playAudio() async {
+      // asset 오디오 파일 실행
+      await _audioPlayer.play(AssetSource('sample_audio_file.wav'));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _playAudio,
+        tooltip: 'Audio Play',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
